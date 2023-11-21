a3200b51b2fd3b260facc66c2c7216a2d9aa3527
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-11-21 13:21:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Statistics from './components/Statistics.js';
+// import LineChart from './components/Charts/LineChart.js';
+import BarChart from './components/Charts/BarChart.js';
+// import PieChart from './components/Charts/PieChart.js';
+// import HeatMap from './components/Charts/HeatMap.js';
+import './styles/App.css';  // Global styles
+import 'bulma/css/bulma.css';  // Bulma styles
+import covidAPI from './services/covidAPI.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [globalStats, setGlobalStats] = useState(null);
+
+	useEffect(() => {
+		const fetchGlobalStats = async () => {
+			try {
+				const response = await covidAPI.getGlobalStatistics();
+				console.log('App - Global Stats:', response);
+				setGlobalStats(response);
+			} catch (error) {
+				console.error('Error fetching global statistics:', error);
+			}
+		};
+
+		fetchGlobalStats();
+	}, []);
+
+	return (
+		<div className="app-container">
+			<header>
+				<h1>COVID-19 Dashboard</h1>
+			</header>
+			<main>
+				<Statistics />
+				{/* <LineChart /> */}
+				<BarChart data={globalStats} />
+				{/* <PieChart /> */}
+				{/* <HeatMap /> */}
+			</main>
+			<footer>
+				<p>&copy; 2023 COVID-19 Dashboard</p>
+			</footer>
+		</div>
+	);
+};
 
 export default App;
